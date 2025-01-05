c0440c152c27171be772cb445d719d3ff99414f6
Ujjval <ujjvalj.nt@gmail.com>
2025-01-05 17:27:36 +0530

    done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,34 @@
-import { Slot, Stack } from "expo-router";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import TimerProvider from "@/context/TimerContext";
+
+// Prevent Splash to auto hide before loading fonts
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
-    </Stack>
+    <TimerProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="(modal)/adjust-meditation-duration"
+          options={{ headerShown: false, presentation: "modal" }}
+        />
+      </Stack>
+    </TimerProvider>
   );
 }
--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,12 +1,81 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MEDITATIONS_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
 import { router, useLocalSearchParams } from "expo-router";
+import { Audio } from "expo-av";
+
+import { MEDITATION_DATA, AUDIO_FILES } from "@/constants/MeditationData";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import CustomButton from "@/components/CustomButton";
+import { TimerContext } from "@/context/TimerContext";
 const Meditate = () => {
   const { id } = useLocalSearchParams();
+
+  const { duration: seccondsRemaining, setDuration } = useContext(TimerContext);
+
+  // const [seccondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setMeditating] = useState(false);
+  const [audioSound, setSound] = useState<Audio.Sound>();
+  const [isPlayingAudio, setPlayingAudio] = useState(false);
+
+  useEffect(() => {
+    if (seccondsRemaining === 0) {
+      setMeditating(false);
+      return;
+    }
+    let timerId: NodeJS.Timeout;
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setDuration(seccondsRemaining - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [seccondsRemaining, isMeditating]);
+
+  useEffect(() => {
+    return () => {
+      setDuration(10);
+      audioSound?.unloadAsync();
+    };
+  }, [audioSound]);
+
+  const toggleMeditationSessionStatus = async () => {
+    if (seccondsRemaining === 0) setDuration(10);
+
+    setMeditating(!isMeditating);
+    await toggleSound();
+  };
+  const toggleSound = async () => {
+    const sound = audioSound ? audioSound : await initializeSound();
+    const status = await sound?.getStatusAsync();
+    if (status?.isLoaded && !isPlayingAudio) {
+      await sound.playAsync();
+      setPlayingAudio(true);
+    } else {
+      await sound.pauseAsync();
+      setPlayingAudio(false);
+    }
+  };
+  const initializeSound = async () => {
+    const audioFileName = MEDITATION_DATA[Number(id) - 1].audio;
+    const { sound } = await Audio.Sound.createAsync(AUDIO_FILES[audioFileName]);
+    setSound(sound);
+    return sound;
+  };
+  const handelAdjustDuration = () => {
+    if (isMeditating) toggleMeditationSessionStatus();
+    router.push("/(modal)/adjust-meditation-duration");
+  };
+  // Format time left
+  const formattedTimeMinutes = String(
+    Math.floor(seccondsRemaining / 60)
+  ).padStart(2, "0");
+
+  const formattedTimeSeconds = String(seccondsRemaining % 60).padStart(2, "0");
   return (
     <View className="flex-1">
       <ImageBackground
@@ -23,16 +92,21 @@ const Meditate = () => {
           </Pressable>
           <View className="flex-1 justify-center">
             <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 items-center justify-center">
-              <Text className="text-4xl text-blue-800 font-semibold">
-                00:00
+              <Text className="text-4xl text-blue-800 font-semibold font-rmono">
+                {formattedTimeMinutes}:{formattedTimeSeconds}
               </Text>
             </View>
-            <View className="mb-5">
-              <CustomButton
-                onPress={() => console.log("press")}
-                title="Start Meditation"
-              />
-            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButton
+              onPress={handelAdjustDuration}
+              title="Adjust Duration"
+            />
+            <CustomButton
+              containerStyles="mt-4"
+              onPress={toggleMeditationSessionStatus}
+              title={isMeditating ? "Stop" : "Start Meditation"}
+            />
           </View>
         </AppGradient>
       </ImageBackground>
